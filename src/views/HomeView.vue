55def24c156f604f5ca903678cd1195bdf6be159
Filip Potocnik <filip.potocnik@gmail.com>
2025-01-31 15:06:45 +0100

    Add basic categories to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const categories: Record<string, string>[] = [
+  {
+    key: 'people',
+    description: 'A person within the Star Wars universe',
+  },
+  {
+    key: 'planets',
+    description: 'A large mass, planet or planetoid in the Star Wars Universe',
+  },
+  {
+    key: 'starships',
+    description: 'A single transport craft that has hyperdrive capability',
+  },
+]
+</script>
 
 <template>
-  <main>yo</main>
+  <RouterLink v-for="category in categories" :to="`/${category.key}`">
+    <div>{{ category.key }}</div>
+    <div>{{ category.description }}</div>
+  </RouterLink>
 </template>
